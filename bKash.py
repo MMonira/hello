c18983c2ff4213672fef7da4eb430a00877bdb85
Monira <156284752+MMonira@users.noreply.github.com>
2024-05-08 10:07:56 +0600

    Update bKash.py

--- bKash.py
+++ bKash.py
@@ -16,5 +16,33 @@ if prompt == 1 :
     print('Enter Receiver bKash Account No:')
     number = int(input())
     if number:
-        print()
- 
+        print('FREE Send Money to 5 Priyo Numbers')
+        print('Dial *247# and select number 9 to find priyo Numbers.')
+        print('Enter Amount')
+        amount = int(input())
+        if amount:
+            print('FREE Send Money to 5 Priyo numbers up to 25,000 Tk. every month.')
+            refer = input('Enter Reference: ')
+            if refer:
+                print('Add up to 5 Priyo Numbers to Send Money for FREE.')
+                print('Send Money')
+                print(f'To: {number} ')
+                print(f'Amount: Tk {amount}')
+                print(f'Reference: {refer}')
+                PIN = int(input('Enter Menu PIN to confirm:'))
+                if PIN:
+                    print('Your payment is Successfull')
+elif prompt == 2:
+    number = int(input('Enter Reference Number:'))
+    if number:
+        amounts = int(input('Enter Amount:'))
+        if amounts:
+            referen = input('Enter Reference: ')
+            if referen:
+                print('Send Money to Non-bKash User: ')
+                print(f'To : {number}')
+                print(f'Amount : {amounts}')
+                print(f'Reference : {referen}')
+                PINS = int(input('Enter Menu PIN to confirm: '))
+                if PINS:
+                    print('Your Money Succesfully send')
