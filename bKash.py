c5807e9b950c459e1acfb827f62f44a1e16cb21c
Monira <156284752+MMonira@users.noreply.github.com>
2024-05-08 09:21:49 +0600

    Add files via upload

---
+++ bKash.py
@@ -0,0 +1,20 @@
+
+print('bKash')
+print('1 Send Money')
+print('2 Send Money To Non-bKash User')
+print('3 Mobile Recharge')
+print('4 Payment')
+print('5 Cash Out')
+print('6 Pay Bill')
+print('7 Microfinance')
+print('8 Download bKash App')
+print('9 My bKash')
+print('10 Reset PIN')
+prompt = int(input())
+
+if prompt == 1 :
+    print('Enter Receiver bKash Account No:')
+    number = int(input())
+    if number:
+        print()
+ 
